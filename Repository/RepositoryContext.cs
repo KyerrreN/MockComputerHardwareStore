5512796c5843b414bab44863eea4254c6341a3f5
KyerrreN <kyerrrenmgt@gmail.com>
2024-10-04 20:31:26 +0300

    Set up the first migration and applied it to a database

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        // CTOR
+        public RepositoryContext(DbContextOptions options) : base (options)
+        { 
+        }
+
+        // Db Sets
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<GraphicsCard>? GraphicsCards { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
