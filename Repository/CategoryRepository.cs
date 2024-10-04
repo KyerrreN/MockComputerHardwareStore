7d1b1c2a06e933a45c4a8e8217f1faaa5cac7267
KyerrreN <kyerrrenmgt@gmail.com>
2024-10-04 22:55:22 +0300

    Add some basic user interfaces and implementations

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(RepositoryContext context) : base(context)
+        {
+            
+        }
+    }
+}
