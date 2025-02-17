80be57a051adfc3cb349a788be9053e05020a3be
KyerrreN <kyerrrenmgt@gmail.com>
2025-02-17 12:31:10 +0300

    Add basic classes for links for future support of HATEOAS

---
+++ Entities/LinkFolder/LinkResourceBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.LinkFolder
+{
+    public class LinkResourceBase
+    {
+        public LinkResourceBase() { }
+
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+}
